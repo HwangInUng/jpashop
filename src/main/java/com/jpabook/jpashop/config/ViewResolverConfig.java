6e5f2014e6a85021dafd69d159675b565442a4c4
HwangInUng <ung6860@naver.com>
2023-10-26 11:20:13 +0900

    init commit

---
+++ src/main/java/com/jpabook/jpashop/config/ViewResolverConfig.java
@@ -0,0 +1,22 @@
+package com.jpabook.jpashop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+public class ViewResolverConfig {
+
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+
+}
